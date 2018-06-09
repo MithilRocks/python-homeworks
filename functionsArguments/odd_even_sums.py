594b4270e7592907e75f2d3cdc28c01bbcec54cd
MithilRocks <mithilbhoras@yahoo.co.in>
2018-06-09 09:58:08 +0530

    new changes

--- functionsArguments/odd_even_sums.py
+++ functionsArguments/odd_even_sums.py
@@ -1,4 +1,4 @@
-def odd_even_sum(num):
+def my_odd_even_sum(num):
     odd_sum, even_sum = 0, 0
     for i in num:
         i = int(i)
@@ -7,9 +7,25 @@ def odd_even_sum(num):
         else:
             even_sum += i
 
+    return odd_sum, even_sum
+
+def ClassOddEvenDigitsSum(num):
+    odd_sum, even_sum = 0, 0
+    while num!=0:
+        rem = num%10
+        if rem & 1 == 1:
+            odd_sum += rem
+        else:
+            even_sum += rem
+
+        num = int(num/10)
+    
+    return odd_sum, even_sum
+
+def main():
+    num = eval(input("Enter your number: "))
+    odd_sum, even_sum = ClassOddEvenDigitsSum(num)
     print("For number {} odd numbers sum is {} and even numbers sum is {}".format(num, odd_sum, even_sum))
 
 if __name__ == "__main__":
-    num = input("Enter your number: ")
-    odd_even_sum(num)
-    
+    main()
